f16cfdad25318307c2c52977bca536871cc6c9c9
timrrrr <54307723+timrrrr@users.noreply.github.com>
2022-10-05 23:32:20 +0300

    Initial commit

---
+++ src/lib/DogFetcher.svelte
@@ -0,0 +1,29 @@
+
+
+<script lang="ts">
+    // import {IResult} from './interfaces/api.interface'
+    let src = "123";
+
+    const fetchDog = () => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then(res => res.json())
+            .then(result => {
+                console.log(result)
+                src = result.message
+            })
+            .catch(err => console.log(err))
+
+        // src = fetch("https://dog.ceo/api/breeds/image/random").json
+    }
+</script>
+
+
+
+<div class="half">
+    <button on:click={fetchDog}>
+        fetch a new dog
+    </button>
+    <img src={src}>
+
+    
+</div>
